5a1c122ed1721c231576ed95141c789c20552377
Anthony Andrade <andradeanthony95@gmail.com>
2023-09-28 13:23:56 -0500

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Table } from "./components/Table";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const originalUsers = useRef([]);
 
   useEffect(() => {
     fetch("https://randomuser.me/api?results=25")
       .then(async (res) => await res.json())
       .then((res) => {
         setUsers(res.results);
-        // originalUsers.current = res.results;
+        originalUsers.current = res.results;
       })
       .catch((err) => {
         console.error(err);
       });
   }, []);
+
+  const handleInputChange = (event) => {
+    const newSearchTerm = event.target.value;
+    console.log(newSearchTerm);
+    setSearchTerm(newSearchTerm);
+    if (newSearchTerm === "") {
+
+      //setUsers(users);
+    } else {
+      filterUsers();
+    }
+  };
+
+
+  const filterUsers = () => {
+    const filtered = originalUsers.current.filter((user) => {
+      return user.location.country.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setUsers(filtered);
+  };
+
   return (
     <div>
       <div className="card text-center text-white bg-secondary">
@@ -24,13 +47,16 @@ function App() {
           <h1>Test User Api</h1>
 
           <div className="d-flex justify-content-center">
-            <button className="btn btn-primary btn-sm mx-2">Colorear Files</button>
+            <button className="btn btn-primary btn-sm mx-2">
+              Colorear Files
+            </button>
             <button className="btn btn-primary btn-sm mx-2">Ordenar</button>
             <button className="btn btn-primary btn-sm mx-2">Resetear</button>
             <div className="col-1">
               <input
                 className="form-control form-control-sm"
                 placeholder="Filtrar por país"
+                onChange={handleInputChange}
               />
             </div>
           </div>
